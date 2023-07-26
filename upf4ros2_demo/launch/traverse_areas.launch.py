9c93f626d2a7f21fe08ca413e676be9b18bf6a4c
Zer0h1nder <lucas.mair@web.de>
2023-07-26 17:00:42 +0200

    Update traverse_areas.launch.py

--- upf4ros2_demo/launch/traverse_areas.launch.py
+++ upf4ros2_demo/launch/traverse_areas.launch.py
@@ -1,11 +1,15 @@
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch_ros.actions import Node
 
-def generate_launch_description():
+def launch_setup(context, *args, **kwargs):
     pkg_name = "upf4ros2_demo"
+    count = int(context.perform_substitution(LaunchConfiguration('count')))
     
-    drone_count = 3
+    drone_count = count
     ld = LaunchDescription()
+    
     for i in range(0,drone_count):
         plan_executor_node = Node(
             package=pkg_name,
@@ -17,15 +21,10 @@ def generate_launch_description():
             emulate_tty=True,
             output='screen')
         ld.add_action(plan_executor_node)
-
+        
     return ld
-    """
-    plan_executor_node = Node(
-        package=pkg_name,
-        executable="plan_executor",
-        output='screen')
 
-    ld = LaunchDescription()
-    ld.add_action(plan_executor_node)
-    return ld"""
-
+def generate_launch_description():
+    return LaunchDescription(
+        [DeclareLaunchArgument('count', default_value='3')] + [OpaqueFunction(function=launch_setup)]
+    )
